157142d26a0b1c41f70da95fbf94127001474966
Willa Bautista <bautista.willa@gmail.com>
2022-03-03 14:36:05 -0800

    Add nav bar, routes, and home page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import App from './App';
 
-const App = () => {
-    return <h1>Mock React App</h1>;
-}
 ReactDOM.render(<App />, document.getElementById('app'));
